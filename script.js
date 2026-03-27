edfa8437338e74b057c41ccdf394c6c5b7f8284b
Nyxsve-C <nyx.sve.c@gmail.com>
2026-03-26 22:57:15 -0600

    Feat(html, css, js): add button, add styling and flex, write function for setting divs and properties

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 const container = document.getElementById('container');
 
-for (let i = 0; i < 16; i++) {
-    const div = document.createElement('div');
-    container.appendChild(div);
+function setSideSize(size = 4) {
+    for (let i = 0; i < size**2; i++) {
+        const flexBasis = 100 / size;
+        const div = document.createElement('div');
+        div.classList.add('squares');
+        div.style.flexBasis = `${flexBasis}%`;
+        div.addEventListener('mouseenter', () => {
+            div.style.backgroundColor = '#4d4da4';
+        });
+        container.appendChild(div);
+    }
 }
+
+setSideSize(10)
