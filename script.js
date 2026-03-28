6781f4a2489c562abec711ca84ce2a1a3b91e740
Nyxsve-C <nyx.sve.c@gmail.com>
2026-03-27 19:17:22 -0600

    Feat(css, js): fine-tune styling, write randomColor function and refactor js

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 const container = document.getElementById('container');
+const button = document.querySelector('button');
+
+function randomColor() {
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    const rgb = `rgb(${r}, ${g}, ${b})`;
+    return rgb;
+}
 
 function setSideSize(size = 4) {
     for (let i = 0; i < size**2; i++) {
-        const flexBasis = 100 / size;
-        const div = document.createElement('div');
-        div.classList.add('squares');
-        div.style.flexBasis = `${flexBasis}%`;
-        div.addEventListener('mouseenter', () => {
-            div.style.backgroundColor = '#4d4da4';
+        const square = document.createElement('div');
+        const flexBasis = 100 / size + '%';
+        let opacity = 0;
+
+        square.classList.add('square');
+        square.style.flexBasis = flexBasis;
+        square.style.opacity = '0';
+
+        ['pointerdown', 'pointerenter'].forEach(ev => {
+            square.addEventListener(ev, e => {
+                opacity += 0.1;
+                square.style.opacity = opacity.toString();
+                square.style.backgroundColor = randomColor();
+                square.releasePointerCapture(e.pointerId);
+            })
         });
-        container.appendChild(div);
+
+        container.appendChild(square);
     }
 }
 
-setSideSize(10)
+setSideSize();
+
+button.addEventListener('click', () => {
+    const size = prompt('Enter side size (0-100)');
+    if (size >= 0 && size <= 100) {
+        container.replaceChildren();
+        setSideSize(size);
+    }
+});
